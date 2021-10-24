ba04451f1f089c8b5de12d822e15246bfe556d19
Your Name <lbookatz@hotmail.com>
2021-10-24 20:00:49 +0100

    first commit

---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() {
+                new Restaurant { Id=1,Name="Lance's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id=1,Name="franch rest", Cuisine = CuisineType.Franch},
+                new Restaurant { Id=1,Name="Mago Grove", Cuisine = CuisineType.Indian}
+            };
+        }
+
+                    
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
